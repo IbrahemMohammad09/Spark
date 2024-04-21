1e28af8de3befe659a3807201db714efa1cbd368
0mar-helal <omarhlal00@gmail.com>
2024-04-21 21:33:10 +0300

    our team section style

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Home from './pages/Home/Home';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Footer } from './pages/Footer/Footer';
-import AboutUs from '../src/pages/AboutUs/AboutUs'
-import NavigationBar from './pages/NavigationBar/NavigationBar';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "swiper/css";
+import { Footer } from "./pages/Footer/Footer";
+import AboutUs from "../src/pages/AboutUs/AboutUs";
+import NavigationBar from "./pages/NavigationBar/NavigationBar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <NavigationBar />
+        <NavigationBar />
         <Routes>
-          <Route path={'/'} element={<Home/>}/>
-          <Route path={'/about_us'} element={<AboutUs />} />
+          <Route path={"/"} element={<Home />} />
+          <Route path={"/about_us"} element={<AboutUs />} />
         </Routes>
         <Footer />
       </BrowserRouter>
