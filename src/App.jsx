c57d4c2bbe3cd601cfb7a8e515f69b9689249ec0
HasanShamaa2004 <hasan.shamaa10@gmail.com>
2024-04-21 16:11:05 +0300

    Add Link and some style css

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Home from './pages/Home/Home';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Footer } from './pages/Footer/Footer';
-import AboutUs from '../src/pages/AboutUs/AboutUs'
-import NavigationBar from './pages/NavigationBar/NavigationBar';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Footer } from "./pages/Footer/Footer";
+import AboutUs from "../src/pages/AboutUs/AboutUs";
+import NavigationBar from "./pages/NavigationBar/NavigationBar";
+import ContactUs from "./components/ContactUs/ContactUs";
+import OurServices from "./components/ServicesSection/OurServices/OurServices";
+import OurTeam from "./components/OurTeam/OurTeam";
+import ProjectsSection from "./components/ProjectsSection/ProjectsSection";
+import DownloadAppSection from "./components/DownloadAppSection/DownloadAppSection";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <NavigationBar />
+        <NavigationBar />
         <Routes>
-          <Route path={'/'} element={<Home/>}/>
-          <Route path={'/about_us'} element={<AboutUs />} />
+          <Route path={"/"} element={<Home />} />
+          <Route path={"/about_us"} element={<AboutUs />} />
+          <Route path={"/contact_us"} element={<ContactUs />} />
+          <Route path={"/our_services"} element={<OurServices />} />
+          <Route path={"/our_team"} element={<OurTeam />} />
+          <Route path={"/our_projects"} element={<ProjectsSection />} />
+          <Route path={"/our_app"} element={<DownloadAppSection />} />
         </Routes>
         <Footer />
       </BrowserRouter>
