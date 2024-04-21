df2c55cbf669665c71cf097cd901aaff2a493000
MoonesMezher <moonesmezher050@gmail.com>
2024-04-21 22:26:21 +0300

    company services feature & language context

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ import { Footer } from './pages/Footer/Footer';
 import AboutUs from '../src/pages/AboutUs/AboutUs'
 import NavigationBar from './pages/NavigationBar/NavigationBar';
 import CompanyServices from './pages/CompanyServices/CompanyServices';
+import ServicesSection from './components/ServicesSection/ServicesSection';
+import OurTeam from './components/OurTeam/OurTeam';
+import ContactUs from './components/ContactUs/ContactUs';
+import ProjectsSection from './components/ProjectsSection/ProjectsSection';
+import DownloadAppSection from './components/DownloadAppSection/DownloadAppSection';
 
 function App() {
   return (
@@ -15,6 +20,11 @@ function App() {
           <Routes>
             <Route path={'/'} element={<Home/>}/>
             <Route path={'/about_us'} element={<AboutUs />} />
+            <Route path={"/contact_us"} element={<ContactUs />} />
+            <Route path={"/our_services"} element={<ServicesSection />} />
+            <Route path={"/our_team"} element={<OurTeam />} />
+            <Route path={"/our_projects"} element={<ProjectsSection />} />
+            <Route path={"/our_app"} element={<DownloadAppSection />} />
             <Route path={'/company-services'} element={<CompanyServices />} />
           </Routes>
           <Footer />
