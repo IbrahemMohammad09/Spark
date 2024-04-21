2d26bcf4d72db4473f75a16aba25c4f76f9e3e16
IbrahemMohammad09 <137792138+IbrahemMohammad09@users.noreply.github.com>
2024-04-21 21:48:22 +0300

    Correction of service section call

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { Footer } from "./pages/Footer/Footer";
 import AboutUs from "../src/pages/AboutUs/AboutUs";
 import NavigationBar from "./pages/NavigationBar/NavigationBar";
 import ContactUs from "./components/ContactUs/ContactUs";
-import OurServices from "./components/ServicesSection/OurServices/OurServices";
+import ServicesSection from "./components/ServicesSection/ServicesSection";
 import OurTeam from "./components/OurTeam/OurTeam";
 import ProjectsSection from "./components/ProjectsSection/ProjectsSection";
 import DownloadAppSection from "./components/DownloadAppSection/DownloadAppSection";
@@ -20,7 +20,7 @@ function App() {
           <Route path={"/"} element={<Home />} />
           <Route path={"/about_us"} element={<AboutUs />} />
           <Route path={"/contact_us"} element={<ContactUs />} />
-          <Route path={"/our_services"} element={<OurServices />} />
+          <Route path={"/our_services"} element={<ServicesSection />} />
           <Route path={"/our_team"} element={<OurTeam />} />
           <Route path={"/our_projects"} element={<ProjectsSection />} />
           <Route path={"/our_app"} element={<DownloadAppSection />} />
