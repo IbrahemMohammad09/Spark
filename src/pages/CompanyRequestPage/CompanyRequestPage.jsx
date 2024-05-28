766dc44bf40349e006ded8eb3521c54b1f4dff24
IbrahemMohammad09 <137792138+IbrahemMohammad09@users.noreply.github.com>
2024-05-28 07:44:27 +0300

    update request

--- src/pages/CompanyRequestPage/CompanyRequestPage.jsx
+++ src/pages/CompanyRequestPage/CompanyRequestPage.jsx
@@ -97,22 +97,9 @@ const CompanyRequestPage = () => {
 
         Axios.post('rest/company_request/', data)
             .then(response => {
-                console.log(response.data);
-                console.log(response.data.message);
-                // const re = JSON.stringify(response.data.message)
-                // console.log(re);
-                // if(re === "Request submitted successfully") {
                 if(response.data.id === 1) {
                     console.log("hi")
-                    localStorage.setItem("1",'hasCompletedRequest');
-                    for (let i = 0; i < localStorage.length; i++) {
-                        // استرجاع اسم المفتاح
-                        const key = localStorage.key(i);
-                        // استرجاع القيمة المرتبطة بالمفتاح
-                        const value = localStorage.getItem(key);
-                        // طباعة اسم المفتاح وقيمته 
-                        console.log(`Key: ${key}, Value: ${value}`);
-                      }
+                    localStorage.setItem('hasCompletedRequest',true);
                     navigate('/completed');
                 } else {
                     setError('The request has already been successfully sent ');
--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -3,17 +3,7 @@ import MainButton from '../../components/SharedComponents/MainButton/MainButton'
 import SEO from '../../components/SharedComponents/SEO/SEO';
 
 const ErrorPage = () => {
-    localStorage.removeItem('hasCompletedRequest');
-    const hasCompletedRequest ="hasCompletedRequest"
-    if(localStorage.getItem(hasCompletedRequest)){console.log("yes")}
-    for (let i = 0; i < localStorage.length; i++) {
-        // استرجاع اسم المفتاح
-        const key = localStorage.key(i);
-        // استرجاع القيمة المرتبطة بالمفتاح
-        const value = localStorage.getItem(key);
-        // طباعة اسم المفتاح وقيمته 
-        console.log(`Key: ${key}, Value: ${value}`);
-      }
+    
     return (
         <div className="error-page zoomIn">
             <SEO title={'Spark | 404'} description={''} name={'Spark'} type={'website'} keywords={["software develpoment", "software engineer", "student services"]} />
